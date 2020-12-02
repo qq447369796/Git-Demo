e711693ec6e5f046b6323793052e32c383c69f4f
qq447369796 <447369796@qq.com>
2020-12-02 13:37:24 +0800

    合并

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -4,5 +4,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git..");
         System.out.println("hello test");
+        System.out.println("hello master");
     }
 }
